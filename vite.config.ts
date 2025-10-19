0ea08e6b238fb26de18012ff08c0e6bba0f131b8
Mista_Zero <166847416+MistaZero07@users.noreply.github.com>
2025-10-18 22:02:44 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Crashspot-frontend/",
   plugins: [react()],
+  base: '/Crashspot-frontend/', // must match repo name exactly
 })
